17a7fb508d1040d8e24fac33c95ae836e8b25eba
Swastik <swas.rishi@gmail.com>
2019-06-04 14:01:56 +0100

    trying to fix unknown tensor_shape error

--- ResNet-18-var/imagenet-resnet-transWino-prune.py
+++ ResNet-18-var/imagenet-resnet-transWino-prune.py
@@ -121,10 +121,10 @@ class Model(ModelDesc):
             # l = BatchNorm('conv1_bn', l)
             l = MaxPooling('pool1', l, 3, stride=2, padding='SAME')
 
-            l_bra = BatchNorm('res2a_bn2a', l)
-            l_bra = WinogradImTrans('WinogradImTrans_2a_2a', l_bra, tf.nn.relu)
+            # l_bra = BatchNorm('res2a_bn2a', l)
+            # l_bra = WinogradImTrans('WinogradImTrans_2a_2a', l_bra, tf.nn.relu)
             # l_bra = WinogradConv('Winograd_W2a_2a', l_bra, 64, 64, mask=mask_dict['Winograd_W2a_2a/W'] if use_mask else None)
-            l_bra = BatchNorm('res2a_bn2b', l_bra)
+            l_bra = BatchNorm('res2a_bn2b', l)
             l_bra = WinogradImTrans('WinogradImTrans_2a_2b', l_bra, tf.nn.relu)
             l_bra = WinogradConv('Winograd_W2a_2b', l_bra, 64, 64, mask=mask_dict['Winograd_W2a_2b/W'] if use_mask else None)
             l_bra = BatchNorm('res2a_bn2c', l_bra)
