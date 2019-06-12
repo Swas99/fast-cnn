e496deb6152e342873edfc939e3f31f699ccb7c4
Swastik <swas.rishi@gmail.com>
2019-06-13 00:09:19 +0100

    trying to fix tensor shape issue

--- ResNet-18-var/winograd_conv.py
+++ ResNet-18-var/winograd_conv.py
@@ -37,30 +37,30 @@ def WinogradConv(x, in_channel, out_channel, mask=None, W_init=None):
 
 def winograd2x2_conv(I, W):
     print (I, W)
-	return winograd2x2_conv_module.winograd2x2_conv(I, W)
+    return winograd2x2_conv_module.winograd2x2_conv(I, W)
 
 def winograd2x2_conv_grad(i1, i2, grad):
     print (i1, i2, grad)
- 	return winograd2x2_conv_grad_module.winograd2x2_conv_grad(i1, i2, grad)
+    return winograd2x2_conv_grad_module.winograd2x2_conv_grad(i1, i2, grad)
 
 @ops.RegisterShape('Winograd2x2Conv')
 def _my_winograd2x2_shape(op):
-	shape1 = op.inputs[0].get_shape().with_rank(5)
-	shape2 = op.inputs[1].get_shape().with_rank(3)
-	B = shape1.dims[1]
-	nH = shape1.dims[2]
-	nW = shape1.dims[3]
-	H = nH * 2
-	W = nW * 2
-	K = shape2.dims[2]
-	return [tf.TensorShape([B, H, W, K])]
+    shape1 = op.inputs[0].get_shape().with_rank(5)
+    shape2 = op.inputs[1].get_shape().with_rank(3)
+    B = shape1.dims[1]
+    nH = shape1.dims[2]
+    nW = shape1.dims[3]
+    H = nH * 2
+    W = nW * 2
+    K = shape2.dims[2]
+    return [tf.TensorShape([B, H, W, K])]
 
 @ops.RegisterGradient('Winograd2x2Conv')
 def _my_matmul_grad(op, grad_output):
- 	input1 = op.inputs[0]
- 	input2 = op.inputs[1]
- 	grad1, grad2 = winograd2x2_conv_grad_module.winograd2x2_conv_grad(input1, input2, grad_output)
- 	return [grad1, grad2]
+    input1 = op.inputs[0]
+    input2 = op.inputs[1]
+    grad1, grad2 = winograd2x2_conv_grad_module.winograd2x2_conv_grad(input1, input2, grad_output)
+    return [grad1, grad2]
 
 
 
